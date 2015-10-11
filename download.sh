c3808f1dab988179d36735ee762ee7603958471f
dimas.crocco <dimas.crocco@gmail.com>
2015-10-10 21:46:18 -0300

    Fixed little bug when moving files around.

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@
 echo Baixando $1
 filename=`youtube-dl -es $1`
 echo "FILENAME=$filename" 1>&2
-youtube-dl $1 --extract-audio --audio-format mp3 --newline -v --no-playlist 1>&2
-mv "$filename"*.mp3 ~/musicas/"$filename".mp3
+youtube-dl $1 -o "%(title)s.mp3" --extract-audio --audio-format mp3 --newline -v --no-playlist 1>&2
+mv *.mp3 ~/musicas
 thunar ~/musicas &
